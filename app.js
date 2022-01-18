7beca33f973557ae9d35e3088eeb2ef47675c63e
이정현 <simba155korea@gmail.com>
2022-01-18 21:32:43 +0900

    UPDATE app.js

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const compression = require('compression');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var memberRouter = require('./routes/member');
+var memberstockRouter = require('./routes/memberstock');
+var newsRouter = require('./routes/news');
+var replyRouter = require('./routes/reply');
+var likestockRouter = require('./routes/likestock');
+var stockRouter = require('./routes/stock');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(compression());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/member', memberRouter);
+app.use('/memberstock', memberstockRouter);
+app.use('/news', newsRouter);
+app.use('/reply', replyRouter);
+app.use('/likestock', likestockRouter);
+app.use('/stock', stockRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
