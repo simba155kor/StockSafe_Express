19f406c0d8c0b80993bad4c90561f08d3249c65d
이정현 <simba155korea@gmail.com>
2022-01-19 21:29:46 +0900

    UPDATE cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const compression = require('compression');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var memberRouter = require('./routes/member');
@@ -16,6 +17,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
